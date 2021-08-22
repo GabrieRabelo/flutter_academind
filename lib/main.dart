b5db8f2309d4d76820550f647035ed9a51417221
gabrierabelo <gabriel.almeida97@gmail.com>
2021-08-22 19:03:58 -0300

    adds image to main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
-  build(context) {
+  Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-                title: Text('EasyList'),
-            ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
         ),
+        body: Card(
+          child: Column(
+            children: <Widget>[
+              Image.asset('assets/food.jpeg'),
+              Text('Food Paradise')
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
